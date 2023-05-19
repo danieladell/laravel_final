dbee75c4561c0ed7bcbc4b8816bb0cc5bbdc0517
Dani <aluade0542@ieselcaminas.org>
2023-05-19 19:29:55 +0200

    Arreglo logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,18 +51,12 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                    <?php session.destroy(); ?>
+                                <a id="navbar" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" 
+                                aria-haspopup="true" aria-expanded="false" v-pre 
+                                    onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
